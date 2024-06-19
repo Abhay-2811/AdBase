f55a7d39c897675f84188c02e5e547df3ba41b7e
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-06-19 21:26:56 +0530

    home:page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,17 +8,13 @@ function Navbar() {
 
   return (
     <navbar >
-    <div className='flex flex-row mx-auto px-[30px] py-[20px] justify-between bg-slate-900 mt-[0px]'>
-      <h1 className='font-bold text-4xl text-blue-400'>BaseADS</h1>
+    <div className='flex flex-row mx-auto px-[30px] py-[20px] justify-between  mt-[0px]'>
+      <h1 className='font-bold text-xl text-blue-400'>
+      <a href='/'>BaseADS</a>  </h1>
 
       <div>
-        <ul className='flex flex-row gap-x-10 font-semibold text-xl text-white'>
-          <li >
-            <a href='/'>Home</a>
-          </li>
-          <li>
-            <a href=''>Explore</a>
-          </li>
+        <ul className='flex flex-row gap-x-10 font-semibold text-xl text-blue-400'>
+      
           <li>
             <a href='/advertise'>PostADS</a>
           </li>
@@ -26,6 +22,8 @@ function Navbar() {
       </div>
        <BlackCreateWalletButton/>
     </div>
+    <hr className='border-t-2 border-blue-400 mt-4' />
+    <hr className='border-t-4 border-blue-400 mt-4' />
   </navbar>
   )
 }
