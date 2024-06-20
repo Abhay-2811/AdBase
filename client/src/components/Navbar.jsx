461dde0f128468f2495b7f2b8b4fd74da40a8382
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-06-20 15:54:01 +0530

    color:fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,10 +10,10 @@ function Navbar() {
     <navbar >
     <div className='flex flex-row mx-auto px-[40px] py-[20px] justify-between items-center mt-[0px]
 bg-gradient-to-r from-slate-900 via-slate-800 to-black '>
-      <div className='flex-1 font-bold text-2xl text-blue-500'>
+      <div className='flex-1 font-bold text-2xl text-blue-400'>
       <a href='/landing'>BaseAds</a>
       </div>
-      <h1 className='font-bold text-2xl text-blue-500'>
+      <h1 className='font-bold text-2xl text-blue-400'>
         <a href='/'>Explore</a>
       </h1>
       <div className='flex-1 flex justify-end'>
