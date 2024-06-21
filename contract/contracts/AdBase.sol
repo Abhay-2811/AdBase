c888325bfd40bd5b2c2249b5b995299feb3e2ee5
Abhay Upadhyay <abhay281103@gmail.com>
2024-06-22 05:05:41 +0530

    contract push

---
+++ contract/contracts/AdBase.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/*
+What does current iteration needs?
+1. Users Create campaign using this contract
+2. Campaign deatils: Owner, Spending limit [Updatable], Ads [Updatable], Region, Name, isActive (Bool)
+ - Issue: How do we know if we hit. spending limit. Sol: Use FE 
+*/
+
+contract AdBase {
+
+    struct Campaign {
+        address creator;
+        string region;
+        string campaignName;
+        uint256 spendingLimit;
+        string[] adCIDs;
+        bool isActive;
+    }
+
+    address public org; 
+    uint256 public campaignCounter;
+
+    mapping(uint256 => Campaign) public campaigns;
+    mapping(address => uint256[]) public userCampaigns;
+    mapping(address => uint256) public payouts;
+
+    uint256 public CPC = 10000 gwei; // approx 0.3 USD 
+    event CampaignCreated(
+        uint256 campaignId,
+        address creator,
+        string region,
+        string campaignName,
+        uint256 spendingLimit,
+        string[] adCIDs
+    );
+
+    event Payout(address developer, uint256 amount);
+
+    modifier onlyOrg() {
+        require(msg.sender == org, "Caller is not the organization");
+        _;
+    }
+
+    constructor(address _org) {
+        org = _org;
+        campaignCounter = 0;
+    }
+
+    function createCampaign(
+        string memory _region,
+        string memory _campaignName,
+        uint256 _spendingLimit,
+        string[] memory _adCIDs
+    ) payable external {
+        require(msg.value == _spendingLimit, "Invalid Amount");
+        campaigns[campaignCounter] = Campaign( msg.sender, _region, _campaignName, _spendingLimit, _adCIDs, true);
+        userCampaigns[msg.sender].push(campaignCounter);
+        campaignCounter++;
+        emit CampaignCreated(
+            campaignCounter,
+            msg.sender,
+            _region,
+            _campaignName,
+            _spendingLimit,
+            _adCIDs 
+        );
+    }
+
+    function pauseCampaign(uint256 campaignId, uint256 _spentAmount) payable external onlyOrg {
+        Campaign storage thisCampaign = campaigns[campaignId];
+        (bool sent, ) = msg.sender.call{value: (thisCampaign.spendingLimit - _spentAmount)}("");
+        require(sent, "Failed to withdraw and pasue campaign!!");
+    }
+
+    // implement aggregation proof here 
+    function payout(address developer, uint256 amount) external onlyOrg {
+        require(address(this).balance > amount, "Not enough balance");
+        (bool sent, ) = developer.call{value: amount}("");
+        require(sent, "Failed to Transfer!!");
+    }
+
+    // Utility function to get campaigns created by a user
+    function getUserCampaigns(address user)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        return userCampaigns[user];
+    }
+
+    function getAds(uint256 campaignId) external view returns (string[] memory){
+        return campaigns[campaignId].adCIDs;
+    }
+
+    
+}
