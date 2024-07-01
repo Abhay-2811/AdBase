af1d812686c90725da354de8c76a9eb69413133f
Abhay Upadhyay <abhay281103@gmail.com>
2024-07-01 06:01:48 +0530

    home page edit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -19,11 +19,13 @@ export function VortexDemo() {
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center"></p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
           <button className="border-2 border-purple-500 text-white px-[10px] py-[10px] font-medium text-xl bg-gradient-to-r from-purple-900 via-black to-black rounded-lg">
-            Create Ad Campaign
+            <a href="/landing">Create Ad Campaign</a>
           </button>
           <button className="px-4 py-2  font-medium border-2 rounded-lg py-[10px] bg-black text-white">
             {" "}
-            <a href="/landing">Dev Docs</a>
+            <a href="https://abhays-organization-1.gitbook.io/adbase-dev-docs/">
+              Dev Docs
+            </a>
           </button>
         </div>
       </Vortex>
